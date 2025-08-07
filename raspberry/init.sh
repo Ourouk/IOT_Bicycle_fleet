df1422b2d3d9a8a27c56c005a8c229097044844a
Andrea Spelgatti <spelgattiandrea@ourouk.be>
2025-08-07 22:08:39 +0200

    Add a basic mosquitto server for esp32 to rpi com

--- raspberry/init.sh
+++ raspberry/init.sh
@@ -1,9 +1,10 @@
 apt-get update -y
-apt-get install -y hostapd
+apt-get install -y hostapd dnsmasq mosquitto
 # Copy files from the folder to root
 cp -r ./root/ /
 systemctl unmask hostapd
-systemctl enable hostapd
-systemctl start hostapd
+systemctl enable --now hostapd
+systemctl enable --now dnsmasq
+systemctl enable --now mosquitto
 # Enable the service to node-red
-systemctl enable nodered
+systemctl enable --now nodered
